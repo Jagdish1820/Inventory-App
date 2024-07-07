81e3f699d4186719f0ebcb1970e4a6073d2176ab
Jagdish <2021ucp1385@mnit.ac.in>
2024-07-08 00:20:16 +0530

    Validating Data

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,28 +1,45 @@
-import path from 'path'
-import ProductModel from '../models/product.model.js'
+import ProductModel from '../models/product.model.js';
 
-export default class ProductController {
+class ProductsController {
+  getProducts(req, res, next) {
+    var products = ProductModel.getAll();
+    res.render('index', { products });
+  }
 
-    getProducts(req, res) {
-        let products = ProductModel.get()
-        console.log(products);
+  getAddProduct(req, res, next) {
+    res.render('new-product', {
+      errorMessage: null,
+    });
+  }
 
-        // // console.log(path.resolve());
-        // return res.sendFile(path.join(path.resolve(),"src",'views',"products.html" ));
-
-        res.render("products", { products: products });
+  postAddProduct(req, res, next) {
+    // validate data
+    const { name, price, imageUrl } = req.body;
+    let errors = [];
+    if (!name || name.trim() == '') {
+      errors.push('Name is required');
+    }
+    if (!price || parseFloat(price) < 1) {
+      errors.push(
+        'Price must be a positive value'
+      );
+    }
+    try {
+      const validUrl = new URL(imageUrl);
+    } catch (err) {
+      errors.push('URL is invalid');
     }
 
-    getAddForm(req, res) {
-        return res.render("new-product");
+    if (errors.length > 0) {
+      return res.render('new-product', {
+        errorMessage: errors[0],
+      });
     }
 
-    addNewProduct(req, res) {
-        //access data from form.
-        console.log(req.body);
-        ProductModel.add(req.body);
-        let products = ProductModel.get();
-        return res.render('products', { products: products });
-    }
-
+    ProductModel.add(req.body);
+    var products = ProductModel.getAll();
+    res.render('index', { products });
+  }
 }
+
+export default ProductsController;
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,23 +1,22 @@
 export default class ProductModel {
-
-  constructor(_id, _name, _desc, _price, _imageUrl) {
-    this.id = _id;
-    this.name = _name;
-    this.desc = _desc;
-    this.price = _price;
-    this.imageUrl = _imageUrl
+  constructor(id, name, desc, price, imageUrl) {
+    this.id = id;
+    this.name = name;
+    this.desc = desc;
+    this.price = price;
+    this.imageUrl = imageUrl;
   }
 
-  static get() {
+  static getAll() {
     return products;
   }
 
   static add(productObj) {
     let newProduct = new ProductModel(
       products.length + 1,
-      productObj.name, 
-      productObj.desc, 
-      productObj.price, 
+      productObj.name,
+      productObj.desc,
+      productObj.price,
       productObj.imageUrl
     );
     products.push(newProduct);
@@ -28,22 +27,22 @@ var products = [
   new ProductModel(
     1,
     'Product 1',
-    'Description for Product 1',
+    'Description for Product 10',
     19.99,
-    'https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg',
+    'https://m.media-amazon.com/images/I/51-nXsSRfZL.SX328_BO1,204,203,200.jpg'
   ),
   new ProductModel(
     2,
     'Product 2',
     'Description for Product 2',
     29.99,
-    'https://m.media-amazon.com/images/I/51xwGSNX-EL._SX356_BO1,204,203,200_.jpg',
+    'https://m.media-amazon.com/images/I/51xwGSNX-EL.SX356_BO1,204,203,200.jpg'
   ),
   new ProductModel(
     3,
     'Product 3',
     'Description for Product 3',
     39.99,
-    'https://m.media-amazon.com/images/I/31PBdo581fL._SX317_BO1,204,203,200_.jpg',
+    'https://m.media-amazon.com/images/I/31PBdo581fL.SX317_BO1,204,203,200.jpg'
   ),
-]
+];
