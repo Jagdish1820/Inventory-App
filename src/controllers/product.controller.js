332ab6d28f26aeb5896080f4f3e7c7c8248d5efe
Jagdish <2021ucp1385@mnit.ac.in>
2024-07-07 17:03:28 +0530

    Creating Controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -6,6 +6,7 @@ export default class ProductController{
     getProducts(req,res){
         let products = ProductModel.get()
         console.log(products)
+        
         // console.log(path.resolve());
         return res.sendFile(path.join(path.resolve(),"src",'views',"products.html" ));
     }
