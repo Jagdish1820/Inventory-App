4917af6346ec8917efd84f56580567ee284d94ca
Jagdish <2021ucp1385@mnit.ac.in>
2024-07-08 16:15:43 +0530

    Using Express Validator

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,28 +1,44 @@
+import { body, validationResult } from "express-validator";
+
 // HoistedDeclaration => a function
 // class
 // assignment declaration
 
-const validateRequest = (req, res, next) => {
+const validateRequest = async (req, res, next) => {
     // validate data
-    const { name, price, imageUrl } = req.body;
-    let errors = [];
-    if (!name || name.trim() == '') {
-        errors.push('Name is required');
-    }
-    if (!price || parseFloat(price) < 1) {
-        errors.push(
-            'Price must be a positive value'
-        );
-    }
-    try {
-        const validUrl = new URL(imageUrl);
-    } catch (err) {
-        errors.push('URL is invalid');
-    }
+    // const { name, price, imageUrl } = req.body;
+    // let errors = [];
+    // if (!name || name.trim() == '') {
+    //     errors.push('Name is required');
+    // }
+    // if (!price || parseFloat(price) < 1) {
+    //     errors.push(
+    //         'Price must be a positive value'
+    //     );
+    // }
+    // try {
+    //     const validUrl = new URL(imageUrl);
+    // } catch (err) {
+    //     errors.push('URL is invalid');
+    // }
 
-    if (errors.length > 0) {
+    // 1. Setup rules for validation.
+    const rules = [
+        body('name').notEmpty().withMessage('Name is required'),
+        body('price').isFloat({ gt: 0 }).withMessage('Price should be a positive value'),
+        body('imageUrl').isURL().withMessage('Invalid url'),
+    ];
+
+    // 2. run those rules.
+    await Promise.all(rules.map(rule => rule.run(req)));
+    
+    // 3. check if there are any errors after running the rules
+    var validationErrors = validationResult(req);
+
+    // 4. if errors, return the error message
+    if (!validationErrors.isEmpty()) {
         return res.render('new-product', {
-            errorMessage: errors[0],
+            errorMessage: validationErrors.array()[0].msg,
         });
     }
     next();
