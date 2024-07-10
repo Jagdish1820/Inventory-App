092df8bd49f14654af8a3ad3b89097fcc55f3044
Jagdish <2021ucp1385@mnit.ac.in>
2024-07-11 01:09:43 +0530

    Securing Application

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ import ejsLayouts from 'express-ejs-layouts';
 import path from 'path';
 import validationMiddleware from './src/middlewares/validation.middleware.js';
 import { uploadFile } from './src/middlewares/file-upload.middleware.js';
+import session from 'express-session';
+import { auth } from './src/middlewares/auth.middleware.js';
+
+
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(session({
+    secret: 'SecretKey',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+})
+);
+
 
 const productsController =
     new ProductsController();
@@ -28,23 +40,27 @@ app.get('/register', userController.getRegister);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.post('/register', userController.postRegister);
-app.get('/', productsController.getProducts);
+app.get('/', auth, productsController.getProducts);
 
 app.get(
     '/add-product',
+    auth,
     productsController.getAddProduct
 );
 
 app.get('/update-product/:id',
+    auth,
     productsController.getUpdateProductView);
 app.post('/delete-product/:id',
+    auth,
     productsController.deleteProduct);
 
 app.post('/', uploadFile.single('imageUrl'),
+    auth,
     validationMiddleware,
     productsController.postAddProduct);
 
-app.post('/update-product',
+app.post('/update-product', auth,
     productsController.postUpdateProduct);
 
 app.listen(3000, () => {
